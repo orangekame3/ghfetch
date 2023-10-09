bdd5b1091a36fc3aed2e9c1e36509886ca154fb8
orangekame3 <miya.org.0309@gmail.com>
2023-10-09 15:06:26 +0900

    update

--- cmd/root.go
+++ cmd/root.go
@@ -23,102 +23,129 @@ THE SOFTWARE.
 package cmd
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"strings"
 
 	"github.com/TheZoraiz/ascii-image-converter/aic_package"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/fatih/color"
+	"github.com/nsf/termbox-go"
 	"github.com/spf13/cobra"
 )
 
 type gitHubUser struct {
-	Login     string `json:"login"`
-	Name      string `json:"name"`
-	Repos     int    `json:"public_repos"`
-	Followers int    `json:"followers"`
-	Following int    `json:"following"`
+	Login                string `json:"login"`
+	Name                 string `json:"name"`
+	Repos                int    `json:"public_repos"`
+	Followers            int    `json:"followers"`
+	Following            int    `json:"following"`
+	TotalStarsEarned     int    `json:"total_star_earned"`
+	TotalCommitsThisYear int    `json:"total_commit_this_year"`
+	TotalPRs             int    `json:"total_pr"`
+	TotalIssues          int    `json:"total_issues"`
 }
 
-var username string
-var highlightColor string
+var (
+	username       string
+	highlightColor string
+	profileData    map[string]interface{}
+	profileString  string
+	accessToken    string
+)
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "ghfetch",
 	Short: "Fetch GitHub user's profile",
-	Run: func(cmd *cobra.Command, args []string) {
-		if username == "" {
-			fmt.Println("Please provide a GitHub username using the --user flag.")
-			return
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if accessToken == "" {
+			return errors.New("ACCESS_TOKEN environment variable is not set!")
 		}
+		paneWidth := getTermWidth() / 2
+		defaultWidth := 50
+		defaultHeight := 25
+		scaleFactor := float64(paneWidth) / float64(defaultWidth)
+		if scaleFactor > 1 {
+			scaleFactor = 1
+		}
+		newWidth := int(float64(defaultWidth) * scaleFactor)
 
+		if username == "" {
+			return errors.New("Please provide a GitHub username using the --user flag.")
+		}
 		flags := aic_package.DefaultFlags()
-		flags.Dimensions = []int{50, 25}
+		flags.Dimensions = []int{newWidth, int(float64(defaultHeight) * scaleFactor)}
 		flags.Colored = true
 		flags.CustomMap = " .-=+#@"
 		asciiArt, err := aic_package.Convert(fmt.Sprintf("https://github.com/%s.png", username), flags)
 		if err != nil {
-			fmt.Println(err)
-			return
+			return err
 		}
-		asciiLines := strings.Split(asciiArt, "\n")
+		leftPane := lipgloss.NewStyle().Width(newWidth).Render(asciiArt)
 
-		user, err := fetchUser(username)
+		user, err := fetchUserWithGraphQL(username, accessToken)
+
 		if err != nil {
-			fmt.Println("Error fetching user information:", err)
-			return
+			return errors.New("Error fetching user information")
 		}
+
+		// Create user info pane
 		titleColor := colorMap[highlightColor].SprintFunc()
-		infoColor := color.New(color.FgWhite).SprintFunc()
+		User := titleColor("User")
+		Name := titleColor("Name")
+		Repos := titleColor("Repos")
+		Followers := titleColor("Followers")
+		Following := titleColor("Following")
+		TotalStarsEarned := titleColor("Total Stars Earnd")
+		TotalCommitsThisYear := titleColor("Total Commit This Year")
+		TotalPRs := titleColor("Total PRs")
+		TotalIssues := titleColor("Total Issues")
 
-		// Displaying username at the top
-		fmt.Printf("%s    %s: %s\n", asciiLines[0], titleColor("GitHub User"), titleColor(user.Login))
+		userInfoPane := []string{
 
-		userInfo := []string{
-			fmt.Sprintf("Name: %s", user.Name),
-			fmt.Sprintf("Repos: %d", user.Repos),
-			fmt.Sprintf("Followers: %d", user.Followers),
-			fmt.Sprintf("Following: %d", user.Following),
+			fmt.Sprintf("  %s: %s", User, username),
+			separator(username),
+			fmt.Sprintf("  %s: %s", Name, user.Name),
+			fmt.Sprintf("  %s: %d", Repos, user.Repos),
+			fmt.Sprintf("  %s: %d", Followers, user.Followers),
+			fmt.Sprintf("  %s: %d", Following, user.Following),
+			fmt.Sprintf("  %s: %d", TotalStarsEarned, user.TotalStarsEarned),
+			fmt.Sprintf("  %s: %d", TotalCommitsThisYear, user.TotalCommitsThisYear),
+			fmt.Sprintf("  %s: %d", TotalPRs, user.TotalPRs),
+			fmt.Sprintf("  %s: %d", TotalIssues, user.TotalIssues),
 		}
+		if profileString != "" {
+			err := json.Unmarshal([]byte(profileString), &profileData)
+			if err != nil {
+				fmt.Println("error parsing profile json:", err)
+				return err
+			}
 
-		for i := 1; i < len(asciiLines) || i-1 < len(userInfo); i++ {
-			left := ""
-			if i < len(asciiLines) {
-				left = asciiLines[i]
+			// Append custom profile data to the userInfoPane
+			for key, value := range profileData {
+				key := titleColor(key)
+				userInfoPane = append(userInfoPane, fmt.Sprintf("  %s: %v", key, value))
 			}
-			right := ""
-			if i-1 < len(userInfo) {
-				splitted := strings.Split(userInfo[i-1], ": ")
-				right = titleColor(splitted[0]) + ": " + infoColor(splitted[1])
-			}
-			fmt.Printf("%-60s    %s\n", left, right)
 		}
+		rightPaneContent := strings.Join(userInfoPane, "\n")
+		rightPane := lipgloss.NewStyle().Width(paneWidth).Render(rightPaneContent)
+
+		fmt.Print(lipgloss.JoinHorizontal(lipgloss.Center, leftPane, rightPane))
+		return nil
 	},
-}
-
-func fetchUser(username string) (*gitHubUser, error) {
-	url := fmt.Sprintf("https://api.github.com/users/%s", username)
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	var user gitHubUser
-	err = json.NewDecoder(resp.Body).Decode(&user)
-	if err != nil {
-		return nil, err
-	}
-	return &user, nil
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
+		log.Println(err)
 		os.Exit(1)
 	}
 }
@@ -127,9 +154,10 @@ func init() {
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	rootCmd.Flags().StringVarP(&username, "user", "u", "", "GitHub username")
 	rootCmd.Flags().StringVarP(&highlightColor, "color", "c", "blue", "Highlight color for text")
+	rootCmd.Flags().StringVarP(&profileString, "profile", "p", "", "Additional user profile in JSON format")
+	rootCmd.PersistentFlags().StringVar(&accessToken, "access-token", "", "Your GitHub access token")
 }
 
-// Color map for user-specified colors
 var colorMap = map[string]*color.Color{
 	"red":     color.New(color.FgRed),
 	"green":   color.New(color.FgGreen),
@@ -137,4 +165,182 @@ var colorMap = map[string]*color.Color{
 	"blue":    color.New(color.FgBlue),
 	"magenta": color.New(color.FgMagenta),
 	"cyan":    color.New(color.FgCyan),
+}
+
+func getTermWidth() int {
+	err := termbox.Init()
+	if err != nil {
+		log.Fatalf("error initializing termbox: %v", err)
+	}
+	defer termbox.Close()
+
+	width, _ := termbox.Size()
+	return width
+}
+
+func separator(value string) string {
+	titleColor := colorMap[highlightColor].SprintFunc()
+	lineLength := 6 + len(value)
+	return titleColor("  " + strings.Repeat("-", lineLength))
+}
+
+func fetchUserWithGraphQL(username, accessToken string) (*gitHubUser, error) {
+	url := "https://api.github.com/graphql"
+	token := accessToken // 実際には環境変数などから取得する
+	var endCursor string
+	var hasNextPage bool = true
+	var totalStars int
+	var totalRepos int
+	var query string
+
+	for hasNextPage {
+		if endCursor == "" {
+			query = fmt.Sprintf(`{
+		user(login: "%s") {
+			name
+			repositories(first: 100, ownerAffiliations: OWNER, isFork: false, orderBy: {direction: DESC, field: STARGAZERS}) {
+				nodes {
+					stargazers {
+						totalCount
+					}
+				}
+				pageInfo {
+					endCursor
+					hasNextPage
+				}
+			}
+			followers {
+				totalCount
+			}
+			following {
+				totalCount
+			}
+			starredRepositories {
+				totalCount
+			}
+			contributionsCollection {
+				totalCommitContributions
+			}
+			pullRequests {
+				totalCount
+			}
+			issues {
+				totalCount
+			}
+		}
+	}`, username)
+		} else {
+			query = fmt.Sprintf(`{
+		user(login: "%s") {
+			name
+			repositories(first: 100, ownerAffiliations: OWNER, isFork: false, orderBy: {direction: DESC, field: STARGAZERS}, after: "%s") {
+				nodes {
+					stargazers {
+						totalCount
+					}
+				}
+				pageInfo {
+					endCursor
+					hasNextPage
+				}
+			}
+			followers {
+				totalCount
+			}
+			following {
+				totalCount
+			}
+			starredRepositories {
+				totalCount
+			}
+			contributionsCollection {
+				totalCommitContributions
+			}
+			pullRequests {
+				totalCount
+			}
+			issues {
+				totalCount
+			}
+		}
+	}`, username, endCursor)
+		}
+
+		body, err := json.Marshal(map[string]string{
+			"query": query,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
+		if err != nil {
+			return nil, err
+		}
+		req.Header.Set("Authorization", "Bearer "+token)
+		req.Header.Set("Content-Type", "application/json")
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return nil, err
+		}
+
+		err = json.NewDecoder(resp.Body).Decode(&result)
+		if err != nil {
+			return nil, err
+		}
+		resp.Body.Close()
+		for _, repo := range result.Data.User.Repositories.Nodes {
+			totalStars += repo.Stargazers.TotalCount
+		}
+		totalRepos += len(result.Data.User.Repositories.Nodes)
+		endCursor = result.Data.User.Repositories.PageInfo.EndCursor
+		hasNextPage = result.Data.User.Repositories.PageInfo.HasNextPage
+	}
+	user := &gitHubUser{
+		Name:                 result.Data.User.Name,
+		Repos:                totalRepos,
+		Followers:            result.Data.User.Followers.TotalCount,
+		Following:            result.Data.User.Following.TotalCount,
+		TotalStarsEarned:     totalStars,
+		TotalCommitsThisYear: result.Data.User.ContributionsCollection.TotalCommitContributions,
+		TotalPRs:             result.Data.User.PullRequests.TotalCount,
+		TotalIssues:          result.Data.User.Issues.TotalCount,
+	}
+
+	return user, nil
+}
+
+var result struct {
+	Data struct {
+		User struct {
+			Name      string `json:"name"`
+			Followers struct {
+				TotalCount int `json:"totalCount"`
+			}
+			Following struct {
+				TotalCount int `json:"totalCount"`
+			}
+			Repositories struct {
+				Nodes []struct {
+					Stargazers struct {
+						TotalCount int `json:"totalCount"`
+					} `json:"stargazers"`
+				} `json:"nodes"`
+				PageInfo struct {
+					EndCursor   string `json:"endCursor"`
+					HasNextPage bool   `json:"hasNextPage"`
+				} `json:"pageInfo"`
+			} `json:"repositories"`
+			PullRequests struct {
+				TotalCount int `json:"totalCount"`
+			}
+			Issues struct {
+				TotalCount int `json:"totalCount"`
+			}
+			ContributionsCollection struct {
+				TotalCommitContributions int `json:"totalCommitContributions"`
+			}
+		} `json:"user"`
+	} `json:"data"`
 }
--- go.mod
+++ go.mod
@@ -4,22 +4,30 @@ go 1.21.0
 
 require (
 	github.com/TheZoraiz/ascii-image-converter v1.13.1
+	github.com/charmbracelet/lipgloss v0.8.0
 	github.com/fatih/color v1.15.0
+	github.com/nsf/termbox-go v1.1.1
 	github.com/spf13/cobra v1.7.0
 )
 
 require (
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/disintegration/imaging v1.6.2 // indirect
 	github.com/fogleman/gg v1.3.0 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/gookit/color v1.4.2 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/makeworld-the-better-one/dither/v2 v2.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/nathan-fiscaletti/consolesize-go v0.0.0-20210105204122-a87d9f614b9d // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
 	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d // indirect
-	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
 )
